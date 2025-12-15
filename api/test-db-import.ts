aa61bf6e890a1426fa448c8c9f327b3635298308
Isaac Morgado <prism.ai.consulting>
2025-12-15 16:37:37 -0500

    Rename to _db-minimal to exclude from Vercel routing

--- api/test-db-import.ts
+++ api/test-db-import.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { hello } from './db-minimal';
+import { hello } from './_db-minimal';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
